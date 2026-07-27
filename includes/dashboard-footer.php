0eea671c715c7b5bbadd1a5ca3d9823be47ca1bc
justine-andrie-v-abella <justineandrieabella@gmail.com>
2026-07-27 14:50:58 +0800

    added a sidebar

--- includes/dashboard-footer.php
+++ includes/dashboard-footer.php
@@ -1,4 +1,6 @@
-</div>
+</div><!-- /.dash-main -->
+</div><!-- /.dash-shell -->
 <script src="assets/js/dashboard.js"></script>
+<script src="assets/js/sidebar-toggle.js"></script>
 </body>
 </html>
